a12a4274bb7d2aa4321c9db77a9226e59bfddd97
Kapil Raina <kapil.raina@in.ibm.com>
2020-02-26 15:31:34 +0530

    Config Server

--- ProductMicroservice/src/main/java/com/ms/bootcamp/ProductMicroservice/GenericController.java
+++ ProductMicroservice/src/main/java/com/ms/bootcamp/ProductMicroservice/GenericController.java
@@ -2,12 +2,14 @@ package com.ms.bootcamp.ProductMicroservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GenericController {
 	@Autowired
 	MinionsLibrary library;
