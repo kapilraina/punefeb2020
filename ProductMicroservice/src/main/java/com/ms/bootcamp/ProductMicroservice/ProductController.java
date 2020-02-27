7bce856ff6828398abec8c68524b48a32fbf92f2
Kapil Raina <kapil.raina@in.ibm.com>
2020-02-27 17:58:50 +0530

    Tracing

--- ProductMicroservice/src/main/java/com/ms/bootcamp/ProductMicroservice/ProductController.java
+++ ProductMicroservice/src/main/java/com/ms/bootcamp/ProductMicroservice/ProductController.java
@@ -3,6 +3,8 @@ package com.ms.bootcamp.ProductMicroservice;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +21,7 @@ import com.ms.bootcamp.ProductMicroservice.service.ProductService;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
-
+	private static Logger log = LoggerFactory.getLogger(ProductController.class);
 	@Autowired
 	private ProductService productService;
 
