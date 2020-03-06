638ced92ca11e9bfa9c25d79c826cf537a22e9f5
Kapil Raina <kapil.raina@in.ibm.com>
2020-03-06 18:33:11 +0530

    sundry

---
+++ ThornTailDemo/src/main/java/com/microservices/microprofile/discountms/model/ProductCategory.java
@@ -0,0 +1,12 @@
+package com.microservices.microprofile.discountms.model;
+
+public enum ProductCategory {
+	FURNISHING,
+	KITCHENELECTRONIC,
+	TOY,
+	MOBILEPHONE,
+	MEDICALEQUIPMENT,
+	MUSICINSTRUMENT
+	
+
+}
